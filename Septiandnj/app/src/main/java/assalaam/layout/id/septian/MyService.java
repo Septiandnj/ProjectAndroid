7317a9a9081ce020c245d66549141dd2b0b7573f
151610237-smkassalaambandung <151610237@smkasssalaambandung.sch.id>
2017-07-29 13:16:10 +0400

    iin

---
+++ Septiandnj/app/src/main/java/assalaam/layout/id/septian/MyService.java
@@ -0,0 +1,31 @@
+package assalaam.layout.id.septian;
+
+/**
+ * Created by Administrator on 7/29/2017.
+ */
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+
+public class MyService extends Service {
+    MediaPlayer mp;
+
+    @Override
+    public IBinder onBind(Intent arg0) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    @Override
+    public void onCreate() {
+        mp=MediaPlayer.create(this, R.raw.despacito);
+        mp.setLooping(false);
+    }
+    public void onStart (Intent intent, int stardId) {
+        mp.start();
+    }
+    @Override
+    public void onDestroy() {
+        mp.stop();
+    }
+}
