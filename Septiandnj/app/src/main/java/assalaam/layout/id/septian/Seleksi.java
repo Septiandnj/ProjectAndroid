ecac9d04fba2139adfc0d968c96dc52305284501
151610237-smkassalaambandung <151610237@smkasssalaambandung.sch.id>
2017-07-31 16:47:28 +0400

    kesalahan

--- Septiandnj/app/src/main/java/assalaam/layout/id/septian/Seleksi.java
+++ Septiandnj/app/src/main/java/assalaam/layout/id/septian/Seleksi.java
@@ -10,21 +10,20 @@ import android.widget.TextView;
 public class Seleksi extends ListActivity {
     /** Called when the activity is first created. */
     TextView seleksi;
-    String[] pilihan = {
-            "Merbabu", "Merapi", "Lawu", "Rinjani",
-            "Sumbing", "Sindoro", "Krakatau", "Selat Sunda", "Selat Bali",
-            "Selat Malaka", "Kalimantan", "Sulawesi", "Jawa" };
+    String[] pilihan={"Merabu","Merapi","Lawu","Rinjani",
+            "Sumbing","Sindaro","Krakatau","Selat Sunda","Selat Bali",
+            "Selat Malaka","Kalimantan","Sulawesi","Jawa"};
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seleksi);
-
         setListAdapter(new ArrayAdapter<String>
-                (this,android.R.layout.simple_list_item_1, pilihan));
-        seleksi = (TextView) findViewById(R.id.yangDipilih);
+                (this,android.R.layout.simple_list_item_1,pilihan));
+        seleksi=(TextView) findViewById(R.id.yangDipilih);
     }
-
-    public void onListItemClick(ListView parent, View v, int position, long id) {
+    public void onListItemClick (ListView parent, View v, int position, long id)
+    {
         seleksi.setText(pilihan[position]);
     }
 }
