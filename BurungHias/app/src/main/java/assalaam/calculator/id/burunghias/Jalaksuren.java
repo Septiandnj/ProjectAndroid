121a91404b9751dc586a9b82c935ee07df7e5ecb
151610237-smkassalaambandung <151610237@smkasssalaambandung.sch.id>
2017-08-09 16:17:36 +0400

    alhamdulillah ahh

--- BurungHias/app/src/main/java/assalaam/calculator/id/burunghias/Jalaksuren.java
+++ BurungHias/app/src/main/java/assalaam/calculator/id/burunghias/Jalaksuren.java
@@ -1,12 +1,21 @@
 package assalaam.calculator.id.burunghias;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
+import java.io.IOException;
+
 public class Jalaksuren extends Activity {
+
+    private Button btnPlay;
+    private Button btnPause;
+    private Button btnStop;
+    private MediaPlayer mp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +31,99 @@ public class Jalaksuren extends Activity {
                 startActivityForResult(myIntent, 0);
             }
         });
+
+        mp = new MediaPlayer();
+
+        btnPlay = (Button) findViewById(R.id.btnPLAY);
+        btnPause = (Button) findViewById(R.id.btnPAUSE);
+        btnStop = (Button) findViewById(R.id.btnSTOP);
+
+        stateAwal();
+
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                play();
+                btnPlay.setEnabled(false);
+                btnPause.setEnabled(true);
+                btnStop.setEnabled(true);
+            }
+        });
+
+        btnPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pause();
+            }
+        });
+
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stop();
+            }
+        });
+    }
+
+    /** State Awal / Pertama Dijalankan */
+    public void stateAwal(){
+        btnPlay.setEnabled(true);
+        btnPause.setEnabled(false);
+        btnStop.setEnabled(false);
+    }
+
+    /** Dijalankan Oleh Tombol Play */
+    private void play() {
+        /** Memanggil File MP3 "indonesiaraya.mp3" */
+        mp = MediaPlayer.create(this, R.raw.jalaksuren);
+
+        try {
+            mp.prepare();
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        /** Menjalankan Audio */
+        mp.start();
+
+        /** Penanganan Ketika Suara Berakhir */
+        mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                stateAwal();
+            }
+        });
+    }
+
+    /** Dijalankan Oleh Tombol Pause */
+    public void pause(){
+        if(mp.isPlaying()){
+            if(mp!=null){
+                mp.pause();
+
+            }
+        } else {
+            if(mp!=null){
+                mp.start();
+
+            }
+        }
+    }
+
+    /** Dijalankan Oleh Tombol Stop */
+    public void stop(){
+        mp.stop();
+
+        try{
+            mp.prepare();
+            mp.seekTo(0);
+        }catch (Throwable t) {
+            t.printStackTrace();
+        }
+
+        stateAwal();
     }
 
 }
